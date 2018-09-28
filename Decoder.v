7014483328828d8e81821eb730dcee10a313028a
willxujun <you@example.com>
2018-09-28 11:27:13 +0800

    first commit

---
+++ Decoder.v
@@ -0,0 +1,162 @@
+`timescale 1ns / 1ps
+/*
+----------------------------------------------------------------------------------
+-- Company: NUS	
+-- Engineer: (c) Shahzor Ahmad and Rajesh Panicker  
+-- 
+-- Create Date: 09/23/2015 06:49:10 PM
+-- Module Name: Decoder
+-- Project Name: CG3207 Project
+-- Target Devices: Nexys 4 (Artix 7 100T)
+-- Tool Versions: Vivado 2015.2
+-- Description: Decoder Module
+-- 
+-- Dependencies: NIL
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------
+--	License terms :
+--	You are free to use this code as long as you
+--		(i) DO NOT post it on any public repository;
+--		(ii) use it only for educational purposes;
+--		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of ARM Holdings or other entities.
+--		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
+--		(v)	acknowledge that the program was written based on the microarchitecture described in the book Digital Design and Computer Architecture, ARM Edition by Harris and Harris;
+--		(vi) send an email to rajesh.panicker@ieee.org briefly mentioning its use (except when used for the course CG3207 at the National University of Singapore);
+--		(vii) retain this notice in this file or any files derived from this.
+----------------------------------------------------------------------------------
+*/
+
+module Decoder(
+    input [3:0] Rd,
+    input [1:0] Op,
+    input [5:0] Funct,
+    output reg PCS,
+    output reg RegW,
+    output reg MemW,
+    output reg MemtoReg,
+    output reg ALUSrc,
+    output reg [1:0] ImmSrc,
+    output reg [1:0] RegSrc,
+    output reg NoWrite,
+    output reg [1:0] ALUControl,
+    output reg [1:0] FlagW
+    );
+    
+    reg ALUOp ;
+    reg [9:0] controls ;
+    //<extra signals, if any>
+    
+    always @(Rd) begin
+        MemtoReg = 0;
+    end
+    
+    always @ (*) begin
+        case(Op)
+            2'b00: begin
+                case(Funct[5])
+                    1'b0: begin
+                        MemtoReg = 0;
+                        MemW = 0;
+                        ALUSrc = 0;
+                        ImmSrc = 2'bXX;
+                        RegW = 1;
+                        RegSrc = 2'b00;
+                        ALUOp = 1;
+                    end
+                    1'b1: begin
+                        MemtoReg = 0;
+                        MemW = 0;
+                        ALUSrc = 1;
+                        ImmSrc = 2'b00;
+                        RegW = 1;
+                        RegSrc = 2'bX0;
+                        ALUOp = 1;
+                    end
+                endcase
+            end
+            2'b01: begin
+                case(Funct[0])
+                    1'b0: begin
+                        MemtoReg = 1'bX;
+                        MemW = 1;
+                        ALUSrc = 1;
+                        ImmSrc = 2'b01;
+                        RegW = 0;
+                        RegSrc = 2'b10;
+                        ALUOp = 0;                        
+                    end
+                        
+                    1'b1: begin
+                        MemtoReg = 1;
+                        MemW = 0;
+                        ALUSrc = 1;
+                        ImmSrc = 2'b01;
+                        RegW = 1;
+                        RegSrc = 2'bX0;
+                        ALUOp = 0;
+                    end                 
+                endcase
+            end
+            2'b10: begin
+                MemtoReg = 0;
+                MemW = 0;
+                ALUSrc = 1;
+                ImmSrc = 2'b10;
+                RegW = 0;
+                RegSrc = 2'bX1;
+                ALUOp = 0;                
+            end
+            2'b11: ;        
+        endcase
+        
+        case(ALUOp)
+            1'b0: begin
+                ALUControl = 2'b00;
+                FlagW = 2'b00;
+            end
+            1'b1: begin
+                case(Funct[4:0])
+                    5'b01000: begin
+                                    ALUControl = 2'b00;
+                                    FlagW = 2'b00;
+                              end
+                    5'b01001: begin
+                                    ALUControl = 2'b00;
+                                    FlagW = 2'b11;
+                              end
+                    5'b00100: begin
+                                    ALUControl = 2'b01;
+                                    FlagW = 2'b00;
+                              end
+                    5'b00101: begin
+                                    ALUControl = 2'b01;
+                                    FlagW = 2'b11;
+                              end
+                    5'b00000: begin
+                                    ALUControl = 2'b10;
+                                    FlagW = 2'b00;
+                              end
+                    5'b00001: begin
+                                    ALUControl = 2'b10;
+                                    FlagW = 2'b10;
+                              end
+                    5'b11000: begin
+                                    ALUControl = 2'b11;
+                                    FlagW = 2'b00;
+                              end
+                    5'b11001: begin
+                                    ALUControl = 2'b11;
+                                    FlagW = 2'b10;
+                              end
+                endcase
+            end
+        endcase
+    end
+    
+endmodule
